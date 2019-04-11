da21facc811739ce53d2512a015ff772fe4c56bf
SurpriZez <rani600@gmail.com>
2019-04-11 20:57:08 +0300

    First Scripts For Project
    Player Script Movement + Gamemanger(first design)

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 speed = new Vector2(0, 0);
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            speed.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed.y += -1;
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed.x += 1;
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed.x += -1;
+        }
+        speed.Normalize();
+        speed *= movementSpeed * Time.fixedDeltaTime;
+        transform.position += (Vector3)speed;
+    }
+}
