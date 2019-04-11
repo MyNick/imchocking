d4cacef254f58fe6c7c31708c5f1399b75aea16d
SurpriZez <rani600@gmail.com>
2019-04-11 22:05:17 +0300

    for itay

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,29 @@ public class GameManager : MonoBehaviour
 {
 
 
-    public int air;
-    public int food;
-    public int water;
+    public List<GameObject> selectedUnits;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            selectedUnits.Add(unit);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach(GameObject unit in selectedUnits)
+            {
+                
+
+            }
+        }
         
     }
 }
